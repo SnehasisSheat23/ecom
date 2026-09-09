c5aceefc829e7d0e27ebd8ef3cf23607cec2b35e
Sandipan Majumder <sandipan.majumder.2005@outlook.com>
2026-09-10 01:20:40 +0530

    update layout by removing background color from various sections for improved consistency

--- Abdullah-Bakheet/about/AboutFounder.tsx
+++ Abdullah-Bakheet/about/AboutFounder.tsx
@@ -14,7 +14,7 @@ export default function AboutFounder() {
                     <h2 className={`text-5xl md:text-6xl lg:text-[100px] uppercase text-[#1a2b25] leading-none tracking-wider scale-y-110 transform origin-bottom ${isArabic ? 'font-sans font-black tracking-tight scale-y-100' : ''}`}>
                         {isArabic ? 'كلمة' : 'Words By'}
                     </h2>
-                    <div className="bg-[#fbdc3c] px-4 md:px-6 pt-2 pb-1">
+                    <div className="px-4 md:px-6 pt-2 pb-1">
                         <h2 className={`text-5xl md:text-6xl lg:text-[100px] uppercase text-[#1a2b25] leading-none tracking-wider scale-y-110 transform origin-bottom ${isArabic ? 'font-sans font-black tracking-tight scale-y-100' : ''}`}>
                             {isArabic ? 'مؤسسنا' : 'Our Founder'}
                         </h2>
